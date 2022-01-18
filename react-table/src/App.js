d682422fd46f63d2115d638752dad0c4c6cda3fb
Pavan Macharla <macharlapavan440@gmail.com>
2022-01-18 09:42:24 +0530

    added project files

---
+++ react-table/src/App.js
@@ -0,0 +1,158 @@
+import React from 'react';
+import './App.css';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+class Home extends React.Component {
+ 
+  render() {
+   
+    return (
+     
+      
+     
+<section class="pricing py-5">
+  <div class="container">
+    <div class="row">
+   
+      <div class="col-lg-4">
+        <div class="card mb-5 mb-lg-0">
+          <div class="card-body">
+            <h5 class="card-title text-muted text-uppercase text-center">Free</h5>
+            <h6 class="card-price text-center">$0<span class="period">/month</span></h6>
+            <hr/>
+            <ul class="fa-ul">
+
+            <li class="mb-3">
+                      <i class="fa fa-check mr-2 text-primary"></i> Single User</li>
+                      <li class="mb-3">
+                      <i class="fa fa-check mr-2 text-primary"></i> 5GB Storage</li>
+                      <li class="mb-3">
+                      <i class="fa fa-check mr-2 text-primary"></i>Unlimited Public Projects </li>
+                      <li class="mb-3">
+                      <i class="fa fa-check mr-2 text-primary"></i>Community Access </li>
+                      <li class="mb-3 text-muted">
+                      <i class="fa fa-times mr-2"></i>
+                      Unlimited
+                Private Projects
+                    </li>
+                    <li class="mb-3 text-muted">
+                      <i class="fa fa-times mr-2"></i>
+                      Dedicated
+                Phone Support
+                    </li>
+                    <li class="mb-3 text-muted">
+                      <i class="fa fa-times mr-2"></i>
+                      Free Subdomain
+                    </li>
+                    <li class="mb-3 text-muted">
+                      <i class="fa fa-times mr-2"></i>
+                      Monthly Status
+                Reports
+                    </li>
+            </ul>
+            <div class="d-grid">
+              <a href="#" class="btn btn-primary text-uppercase">Button</a>
+            </div>
+          </div>
+        </div>
+      </div>
+     
+      <div class="col-lg-4">
+        <div class="card mb-5 mb-lg-0">
+          <div class="card-body">
+            <h5 class="card-title text-muted text-uppercase text-center">Plus</h5>
+            <h6 class="card-price text-center">$9<span class="period">/month</span></h6>
+            <hr/>
+            <ul class="fa-ul">
+              
+            
+
+<li class="mb-3">
+                      <i class="fa fa-check mr-2 text-primary"></i> 5 User</li>
+                      <li class="mb-3">
+                      <i class="fa fa-check mr-2 text-primary"></i> 50GB Storage</li>
+                      <li class="mb-3">
+                      <i class="fa fa-check mr-2 text-primary"></i>Unlimited Public Projects </li>
+                      <li class="mb-3">
+                      <i class="fa fa-check mr-2 text-primary"></i>Community Access </li>
+                     
+                      <li class="mb-3">
+                      <i class="fa fa-check mr-2 text-primary"></i>Unlimited Private Projects</li>
+
+                      <li class="mb-3">
+                      <i class="fa fa-check mr-2 text-primary"></i>Dedicated
+                Phone Support</li>
+
+                <li class="mb-3">
+                      <i class="fa fa-check mr-2 text-primary"></i> Free Subdomain</li>
+                     
+                   
+                   
+                    <li class="mb-3 text-muted">
+                      <i class="fa fa-times mr-2"></i>
+                      Monthly Status
+                Reports
+                    </li>
+            </ul>
+            <div class="d-grid">
+              <a href="#" class="btn btn-primary text-uppercase">Button</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    
+      <div class="col-lg-4">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title text-muted text-uppercase text-center">Pro</h5>
+            <h6 class="card-price text-center">$49<span class="period">/month</span></h6>
+            <hr/>
+            <ul class="fa-ul">
+
+            <li class="mb-3">
+            <i class="fa fa-check mr-2 text-primary"></i> Unlimited User</li>
+                      <li class="mb-3">
+                      <i class="fa fa-check mr-2 text-primary"></i> 150GB Storage</li>
+                      <li class="mb-3">
+                      <i class="fa fa-check mr-2 text-primary"></i>Unlimited Public Projects </li>
+                      <li class="mb-3">
+                      <i class="fa fa-check mr-2 text-primary"></i>Community Access </li>
+                     
+                      <li class="mb-3">
+                      <i class="fa fa-check mr-2 text-primary"></i>Unlimited Private Projects</li>
+
+                      <li class="mb-3">
+                      <i class="fa fa-check mr-2 text-primary"></i>Dedicated
+                Phone Support</li>
+
+                <li class="mb-3">
+                      <i class="fa fa-check mr-2 text-primary"></i> Free Subdomain</li>
+
+                      <li class="mb-3">
+                      <i class="fa fa-check mr-2 text-primary"></i>  Monthly Status
+                Reports</li>
+                     
+                   
+                   
+                   
+
+            
+            </ul>
+            <div class="d-grid">
+              <a href="#" class="btn btn-primary text-uppercase">Button</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+     
+      
+)
+};
+}
+
+export default Home;
